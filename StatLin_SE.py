b452fc44e3f46683652fa039aaab539b869e5cde
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-03-29 09:45:14 -0300

    Create Line object

--- StatLin_SE.py
+++ StatLin_SE.py
@@ -2,10 +2,21 @@
 
 class Measurement:
 
-    def __init__(self, data):
+    def __init__(self, meas_data):
         origin = 1
         destiny = 1
         value = 1
         std_dev = 1
 
+
+class Line:
+
+    def __init__(self, line_data):
+        origin = 1
+        destiny = 1
+        R = 1
+        X = 1
+        B =1
+
+        
 
