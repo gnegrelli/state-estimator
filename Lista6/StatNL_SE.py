903cc3f7d0fc934b72df73fdb9f632aa521f2088
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-04-23 11:50:42 -0300

    Fix bugs in H_q

--- Lista6/StatNL_SE.py
+++ Lista6/StatNL_SE.py
@@ -244,11 +244,11 @@ for line in lines.values():
         for bus in buses.values():
             if bus.ID == line.origin:
                 H_p = np.hstack((H_p, 2*buses[str(line.origin)].V*np.real(Ybus[line.origin - 1, line.destiny - 1]) - buses[str(line.destiny)].V*(np.real(Ybus[line.origin - 1, line.destiny - 1])*np.cos(buses[str(line.origin)].theta + buses[str(line.origin)].theta) - np.imag(Ybus[line.origin - 1, line.destiny - 1])*np.sin(buses[str(line.origin)].theta - buses[str(line.origin)].theta))))
-                H_q = np.hstack((H_q, -2*buses[str(line.origin)].V*(np.real(Ybus[line.origin - 1, line.destiny - 1]) + line.B/2) + buses[str(line.destiny)].V*(-np.real(Ybus[line.origin - 1, line.destiny - 1])*np.sin(buses[str(line.origin)].theta + buses[str(line.origin)].theta) + np.imag(Ybus[line.origin - 1, line.destiny - 1])*np.cos(buses[str(line.origin)].theta - buses[str(line.origin)].theta))))
+                H_q = np.hstack((H_q, -2*buses[str(line.origin)].V*(np.imag(Ybus[line.origin - 1, line.destiny - 1]) + line.B/2) + buses[str(line.destiny)].V*(-np.real(Ybus[line.origin - 1, line.destiny - 1])*np.sin(buses[str(line.origin)].theta + buses[str(line.origin)].theta) + np.imag(Ybus[line.origin - 1, line.destiny - 1])*np.cos(buses[str(line.origin)].theta - buses[str(line.origin)].theta))))
 
             else:
                 H_p = np.hstack((H_p, -buses[str(line.origin)].V*(np.real(Ybus[line.origin - 1, line.destiny - 1])*np.cos(buses[str(line.origin)].theta - buses[str(line.origin)].theta) + np.imag(Ybus[line.origin - 1, line.destiny - 1])*np.sin(buses[str(line.origin)].theta - buses[str(line.origin)].theta))))
-                H_q = np.hstack((H_q, buses[str(line.origin)].V*(-np.real(Ybus[line.origin - 1, line.destiny - 1])*np.cos(buses[str(line.origin)].theta - buses[str(line.origin)].theta) + np.imag(Ybus[line.origin - 1, line.destiny - 1])*np.sin(buses[str(line.origin)].theta - buses[str(line.origin)].theta))))
+                H_q = np.hstack((H_q, buses[str(line.origin)].V*(-np.real(Ybus[line.origin - 1, line.destiny - 1])*np.sin(buses[str(line.origin)].theta - buses[str(line.origin)].theta) + np.imag(Ybus[line.origin - 1, line.destiny - 1])*np.cos(buses[str(line.origin)].theta - buses[str(line.origin)].theta))))
 
 
 for bus in buses.values():
@@ -276,7 +276,7 @@ z = np.hstack((z_p, z_q, z_v))
 # Calculated values of measurements
 h = np.hstack((h_p, h_q, h_v))
 
-# Covariance Matrxi
+# Covariance Matrix
 W = np.zeros((len(z), len(z)))
 np.fill_diagonal(W, np.hstack((w_p, w_q, w_v)))
 
