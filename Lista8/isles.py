8c99c1b96c9ceafb17d9f8d1a5313d0141402bed
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-05-14 15:41:24 -0300

    Decompose matrix G using LU decomposition

--- Lista8/isles.py
+++ Lista8/isles.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy.linalg
 
-z = np.array([0, 0, 0, 0])
+z = np.array([[0, 0, 0, 0]])
 
 H = np.array([[1., 0., -1., 0., 0.],
               [-1., 0., 1., 0., 0.],
@@ -9,22 +9,23 @@ H = np.array([[1., 0., -1., 0., 0.],
               [0., -1., -1., 3., -1.]])
 
 G = np.dot(H.T, H)
+G_f = np.zeros_like(G)
 
-G_f = np.around(scipy.linalg.lu(G)[2], 2)
+P, L, U = scipy.linalg.lu(G)
 
-print(G_f)
-
-counter = 0
-for row in range(5):
-    if np.diag(G_f)[row] != 0:
-        G_f[row] = G_f[row]/np.diag(G_f)[row]
-    elif row != 4:
+null_pivot = 0
+for row in range(len(U)):
+    if np.diag(U)[row] != 0:
+        G_f[row] = U[row]/np.diag(U)[row]
+    else:
         G_f[row, row] = 1
+
         aux = np.zeros(5)
         aux[row] = 1
         H = np.vstack((H, aux))
-        z = np.hstack((z, counter*np.ones(1)))
-        counter += 1
+
+        z = np.hstack((z, np.array([[null_pivot]])))
+        null_pivot += 1
 
 print(G_f)
 
