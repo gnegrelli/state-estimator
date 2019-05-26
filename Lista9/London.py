ac040bc42edfcc6de0e966dd7f6cc9aab66e4111
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-05-26 15:30:26 -0300

    Create factors matrix

--- Lista9/London.py
+++ Lista9/London.py
@@ -17,20 +17,31 @@ H = np.array([[1, -1, 0, 0, 0, 0],
 
 H = H.T
 
+factors = np.zeros((H.shape[0], H.shape[0] - 1))
+
+# Gaussian decomposition adding pseudo-measurements
 for i in range(H.shape[0] - 1):
+
+    # Check if diagonal is null
     if H[i, i] == 0:
+
+        # If the entire row is null add a pseudo-measurement
         if not H[i].any():
             zeros = np.zeros((H.shape[0], 1))
             zeros[i] = 1
             H = np.hstack((H, zeros))
+
+        # Swap columns and continue decomposition
         col = np.where(H[i] != 0)[0][0]
         aux = H[:, i]
         H[:, i] = H[:, col]
         H[:, col] = aux
+
     else:
         H[i] = H[i]/H[i, i]
-    for j in range(i + 1, H.shape[0]):
 
+    for j in range(i + 1, H.shape[0]):
         H[j] = -H[j, i]/H[i, i]*H[i] + H[j]
 
 print(H)
+print(factors)
