b2d599a4e0721233bcf345025b4aece4f94cfc41
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-05-26 19:31:09 -0300

    Add pseudomeasurements to restore observability

--- Lista9/London.py
+++ Lista9/London.py
@@ -1,19 +1,29 @@
 import numpy as np
 
-H = np.array([[-1, 0, 1, 0, 0],
-              [1, 0, -1, 0, 0],
-              [-2, 1, 1, 0, 0],
-              [0, 1, 1, -3, 1]])
+H = np.array([[1, 0, -1, 0, 0],
+              [-1, 0, 1, 0, 0],
+              [2, -1, -1, 0, 0],
+              [0, -1, -1, 3, -1]], dtype=float)
 
-H = np.array([[1, -1, 0, 0, 0, 0],
-              [0, 1, -1, 0, 0, 0],
-              [0, 0, 0, 1, -1, 0],
-              [0, 0, 0, -1, 1, 0],
-              [0, 0, 0, 1, 0, -1],
-              [2, -1, -1, 0, 0, 0],
-              [-1, -1, 3, -1, 0, 0],
-              [0, 0, 0, -1, 1, 0],
-              [0, 0, 0, -1, 0, 1]])
+# H = np.array([[1, -1, 0, 0, 0, 0],
+#               [1, 0, -1, 0, 0, 0],
+#               [-1, 1, 0, 0, 0, 0],
+#               [0, -1, 1, 0, 0, 0],
+#               [0, 0, 0, -1, 1, 0],
+#               [0, 0, 0, -1, 0, 1],
+#               [2, -1, -1, 0, 0, 0],
+#               [0, 0, 0, -1, 1, 0],
+#               [0, 0, 0, -1, 0, 1]], dtype=float)
+
+# H = np.array([[1, -1, 0, 0, 0, 0],
+#               [0, 1, -1, 0, 0, 0],
+#               [0, 0, 0, 1, -1, 0],
+#               [0, 0, 0, -1, 1, 0],
+#               [0, 0, 0, 1, 0, -1],
+#               [2, -1, -1, 0, 0, 0],
+#               [-1, -1, 3, -1, 0, 0],
+#               [0, 0, 0, -1, 1, 0],
+#               [0, 0, 0, -1, 0, 1]], dtype=float)
 
 # H = np.array([[5, -3, -2, 0, 0, 0],
 #               [-3, 2, 1, 0, 0, 0],
@@ -26,18 +36,27 @@ H = H.T
 
 factors = np.zeros((H.shape[0], H.shape[0] - 1))
 
+# List of pseudomeasurements
+pseudomeas = [np.array([[1, -1, 0, 0, 0]]).T, np.array([[0, 0, 1, -1, 0]]).T, np.array([[-1, -1, 3, -1, 0]]).T]
+
+# pseudomeas = [np.array([[-1, -1, 3, -1, 0, 0]]).T]
+
 # Gaussian decomposition adding pseudo-measurements
 for i in range(H.shape[0] - 1):
 
     # Check if diagonal is null
     if H[i, i] == 0:
 
-        # If the entire row is null add a pseudo-measurement
-        if not H[i].any():
+        # While the entire row is null add a pseudo-measurement from list
+        while not H[i].any():
+            add_col = pseudomeas.pop(0)
+            for j in range(i):
+                add_col[j] = np.dot(np.hstack((factors, add_col))[j], add_col)
+            H = np.hstack((H, add_col))
             # break
-            zeros = np.zeros((H.shape[0], 1))
-            zeros[i] = 1
-            H = np.hstack((H, zeros))
+
+        # if not np.where(H[i] != 0)[0].size:
+        #     print("TRETA")
 
         # Swap columns and continue decomposition
         col = np.where(H[i] != 0)[0][0]
@@ -45,11 +64,8 @@ for i in range(H.shape[0] - 1):
         H[:, i] = H[:, col]
         H[:, col] = aux
 
-        factors[i, i] = H[i, i]
-
-    else:
-        factors[i, i] = H[i, i]
-        H[i] = H[i]/H[i, i]
+    factors[i, i] = 1/H[i, i]
+    H[i] = H[i]/H[i, i]
 
     for j in range(i + 1, H.shape[0]):
         factors[j, i] = -H[j, i]/H[i, i]
@@ -57,3 +73,4 @@ for i in range(H.shape[0] - 1):
 
 print(H)
 print(factors)
+# print(pseudomeas)
