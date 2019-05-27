3be6b049829bad1d4213cab57dee540afc9d5e75
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-05-27 12:31:47 -0300

    Exit loop if all pseudo-measurements were added
    The Gaussian decomposition process is ended if all pseud-measurements were added and ther is still a zero pivot before position 'n'

--- Lista9/London.py
+++ Lista9/London.py
@@ -15,15 +15,15 @@ H = np.array([[1, 0, -1, 0, 0],
 #               [0, 0, 0, -1, 1, 0],
 #               [0, 0, 0, -1, 0, 1]], dtype=float)
 
-# H = np.array([[1, -1, 0, 0, 0, 0],
-#               [0, 1, -1, 0, 0, 0],
-#               [0, 0, 0, 1, -1, 0],
-#               [0, 0, 0, -1, 1, 0],
-#               [0, 0, 0, 1, 0, -1],
-#               [2, -1, -1, 0, 0, 0],
-#               [-1, -1, 3, -1, 0, 0],
-#               [0, 0, 0, -1, 1, 0],
-#               [0, 0, 0, -1, 0, 1]], dtype=float)
+H = np.array([[1, -1, 0, 0, 0, 0],
+              [0, 1, -1, 0, 0, 0],
+              [0, 0, 0, 1, -1, 0],
+              [0, 0, 0, -1, 1, 0],
+              [0, 0, 0, 1, 0, -1],
+              [2, -1, -1, 0, 0, 0],
+              [-1, -1, 3, -1, 0, 0],
+              [0, 0, 0, -1, 1, 0],
+              [0, 0, 0, -1, 0, 1]], dtype=float)
 
 # H = np.array([[5, -3, -2, 0, 0, 0],
 #               [-3, 2, 1, 0, 0, 0],
@@ -38,6 +38,7 @@ factors = np.zeros((H.shape[0], H.shape[0] - 1))
 
 # List of pseudomeasurements
 pseudomeas = [np.array([[1, -1, 0, 0, 0]]).T, np.array([[0, 0, 1, -1, 0]]).T, np.array([[-1, -1, 3, -1, 0]]).T]
+added_meas = 0
 
 # pseudomeas = [np.array([[-1, -1, 3, -1, 0, 0]]).T]
 
@@ -49,17 +50,25 @@ for i in range(H.shape[0] - 1):
 
         # While the entire row is null add a pseudo-measurement from list
         while not H[i].any():
-            add_col = pseudomeas.pop(0)
+            try:
+                added_meas += 1
+                print("Adding pseudo-measurement #%d" % added_meas)
+                add_col = pseudomeas.pop(0)
+            except IndexError:
+                print("All pseudo-measurements available were added. System is not observable.")
+                break
             for j in range(i):
                 add_col[j] = np.dot(np.hstack((factors, add_col))[j], add_col)
             H = np.hstack((H, add_col))
-            # break
 
         # if not np.where(H[i] != 0)[0].size:
         #     print("TRETA")
 
         # Swap columns and continue decomposition
-        col = np.where(H[i] != 0)[0][0]
+        try:
+            col = np.where(H[i] != 0)[0][0]
+        except IndexError:
+            break
         aux = np.array(H[:, i])
         H[:, i] = H[:, col]
         H[:, col] = aux
